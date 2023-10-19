cdd5ddc8fe68af1757c59651f5673d0bde8c1538
nasre21 <nasou2001@gmail.com>
2023-10-19 10:53:18 +0200

    its done form

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PasswordPatternDirective } from './directives/password-pattern.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PasswordPatternDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
